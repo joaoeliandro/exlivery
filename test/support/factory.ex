fee3a8a5cf344a88b134c83d0251b4c656f48dc3
João Eliandro Germano <eliandrogermano@gmail.com>
2021-05-13 23:45:39 -0300

    Added factory to use ex machina on the tests

---
+++ test/support/factory.ex
@@ -0,0 +1,42 @@
+defmodule Exlivery.Factory do
+  use ExMachina
+
+  alias Exlivery.Users.User
+  alias Exlivery.Orders.{Order, Item}
+
+  def user_factory do
+    %User{
+      name: "Joao Eliandro",
+      email: "eliandrogermano@gmail.com",
+      cpf: "12345678900",
+      age: 22,
+      address: "Rua Sao Jose, 61"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      category: :pizza,
+      description: "Pizza de calabresa",
+      quantity: 1,
+      unity_price: Decimal.new("28.00")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua Sao Jose, 61",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki de salmao",
+          category: :japanese,
+          quantity: 5,
+          unity_price: Decimal.new("30.00")
+        )
+      ],
+      total_price: Decimal.new("178.00"),
+      user_cpf: "12345678900"
+    }
+  end
+end
