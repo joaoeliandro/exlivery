92748600aac88a2e0ad6366bebdbf45b3df017a6
joaoeliandro <eliandrogermano@gmail.com>
2021-07-05 02:43:46 -0300

    Added orders agent and create or update facade

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,10 +1,14 @@
 defmodule Exlivery do
+  alias Exlivery.Orders.Agent, as: OrdersAgent
+  alias Exlivery.Orders.CreateOrUpdate, as: OrdersCreateOrUpdate
   alias Exlivery.Users.Agent, as: UserAgent
-  alias Exlivery.Users.CreateOrUpdate
+  alias Exlivery.Users.CreateOrUpdate, as: UserCreateOrUpdate
 
   def start_agents do
     UserAgent.start_link(%{})
+    OrdersAgent.start_link(%{})
   end
 
-  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+  defdelegate create_or_update_user(params), to: UserCreateOrUpdate, as: :call
+  defdelegate create_or_update_orders(params), to: OrdersCreateOrUpdate, as: :call
 end
